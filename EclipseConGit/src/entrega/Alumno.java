c05c371a0e1e52b920e614e9099ad0034358dc94
Dante <dante_dantespik@hotmail.com>
2022-04-28 13:31:26 +0200

    Cuarto commit - Creación del constructor con 3 parámetros.

--- EclipseConGit/src/entrega/Alumno.java
+++ EclipseConGit/src/entrega/Alumno.java
@@ -2,4 +2,10 @@ package entrega;
 
 public class Alumno {
 	private String nombre, apellido, dni;
+
+	public Alumno(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
 }
