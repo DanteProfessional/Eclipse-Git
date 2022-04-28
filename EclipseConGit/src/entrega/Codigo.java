bf47adf76568bb5ae879f1a018c624daf4bf5a3a
Dante <dante_dantespik@hotmail.com>
2022-04-28 15:57:46 +0200

    Merge con la rama 'dante'.

--- EclipseConGit/src/entrega/Codigo.java
+++ EclipseConGit/src/entrega/Codigo.java
@@ -5,8 +5,8 @@ public class Codigo {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1º DAW.");
 		System.out.println("Alumno 1: ");
+		
+		Alumno alumno1 = new Alumno("Dante","Pepa", "X6957483N");
+		System.out.println(alumno1);
 	}
-	
-	Alumno alumno1 = new Alumno("Dante","Pepa", "X6957483N");
-	System.out.println(alumno1);
 }
